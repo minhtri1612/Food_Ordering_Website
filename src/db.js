6e2447033606ce0c8f9d0a41d5988b7bf97bd437
minhtri1612 <10422119@student.vgu.edu.vn>
2025-06-12 21:35:45 +0700

    add

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const dbConfig = {
     host: 'localhost',
     user: 'root',          // Replace with your MySQL username
-    password: '123456', // Replace with your MySQL password
+    password: 'Minhchau3112...', // Replace with your MySQL password
     database: 'foodapp'
 };
 
